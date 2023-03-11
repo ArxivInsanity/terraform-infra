5f3267030251a08f4572e375b2e2996bd5398110
Gagan-K-Shetty <gaka4787@colorado.edu>
2023-03-11 14:08:48 -0700

    AR-2 Adding code to use variables file

--- variables.tf
+++ variables.tf
@@ -1,19 +1,16 @@
 variable "project" {
   type        = string
   description = "The google cloud project name"
-  default     = "arxiv-sanity-380223"
 }
 
 variable "region" {
   type        = string
   description = "The region for deployment"
-  default     = "us-west2"
 }
 
 variable "zone" {
   type        = string
   description = "The availability zone for the deployment"
-  default     = "us-west2-a"
 }
 
 variable "GOOGLE_CREDENTIALS" {}
